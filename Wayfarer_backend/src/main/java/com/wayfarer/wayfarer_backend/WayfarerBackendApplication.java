1b222ee291a10c0a57447433312e634c0b893249
Baptiste COL <baptiste.col@fiducial.dom>
2026-06-23 12:47:55 +0200

    First commit backend

---
+++ Wayfarer_backend/src/main/java/com/wayfarer/wayfarer_backend/WayfarerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wayfarer.wayfarer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayfarerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WayfarerBackendApplication.class, args);
+    }
+
+}
---
+++ Wayfarer_backend/src/test/java/com/wayfarer/wayfarer_backend/WayfarerBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.wayfarer.wayfarer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class WayfarerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
